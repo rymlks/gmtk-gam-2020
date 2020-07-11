005e4447ad2dfc5d5871c4e5b17920d814234676
rymlks <rymlks@gmail.com>
2020-07-11 00:40:46 -0400

    add more stuff

--- Assets/Scripts/DisableOnClick.cs
+++ Assets/Scripts/DisableOnClick.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class DisableOnClick : MonoBehaviour
 {
     public Sprite disabledSprite;
+
     private Button buttonComponent;
+    private Sprite enabledSprite;
     private void Start() {
         buttonComponent = GetComponent<Button>();
         buttonComponent.onClick.AddListener(TaskOnClick);
+        enabledSprite = GetComponent<Image>().sprite;
     }
     // Start is called before the first frame update
     void TaskOnClick() {
@@ -18,5 +21,11 @@ public class DisableOnClick : MonoBehaviour
         GetComponent<Image>().color = new Color(1, 1, 1, 0.75f);
         if (disabledSprite != null)
             GetComponent<Image>().sprite = disabledSprite;
+    }
+
+    public void ReEnable() {
+        GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        GetComponent<Image>().sprite = enabledSprite;
+        buttonComponent.enabled = true;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,49 +6,80 @@ public class PlayerController : MonoBehaviour {
     public GameObject manaBar;
     public GameObject gemMenuPanel;
     public float runSpeed;
+    public float manaDecay;
     public float maxMana;
 
+    public float jumpHeight;
+
+    public Transform groundCheckTop_Left;
+    public Transform groundCheckBottomRight;
+    public LayerMask groundLayers;
+
     private Rigidbody2D rigidBody;
+    private bool grounded = true;
+
     private float mana;
-    private Vector3 previousPosition;
     private List<string> disabledKeys;
 
-    private bool toJump = false;
+    private Dictionary<string, GameObject> UIButtons;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         mana = maxMana;
-        previousPosition = transform.position;
         gemMenuPanel.SetActive(false);
         disabledKeys = new List<string>();
+        UIButtons = new Dictionary<string, GameObject>();
+
+        foreach(Transform UIChild in gemMenuPanel.transform) {
+            UIButtons[UIChild.name.ToLower()] = UIChild.gameObject;
+        }
+    }
+
+    void FixedUpdate() {
+        grounded = Physics2D.OverlapArea(groundCheckTop_Left.position, groundCheckBottomRight.position, groundLayers);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (disabledKeys.Find(x => x.Equals(collision.tag.ToLower())) != null) {
+            ReclaimKey(collision.tag);
+            Destroy(collision.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update() {
-        if (GetKeyPress(KeyCode.Space)) {
-            Jump();
-        }
 
-        if (GetKeyPress(KeyCode.E)) {
-            ToggleMenu();
-        }
-        //rigidBody.MovePosition(new Vector2(transform.position.x + runSpeed * Time.deltaTime, transform.position.y));
-
-        mana -= (transform.position.x - previousPosition.x);
+        mana -= manaDecay * Time.deltaTime;
 
         if (mana <= 0) {
             GameOver();
             manaBar.transform.localScale = new Vector3(0.0f, 1.0f, 1.0f);
         } else {
-            rigidBody.velocity = new Vector2(runSpeed, rigidBody.velocity.y);
+            if (GetKeyPress(KeyCode.Space)) {
+                Jump();
+            }
+
+            if (GetKeyPress(KeyCode.E)) {
+                ToggleMenu();
+            }
+
+            if (GetKeyDown(KeyCode.D)) {
+                rigidBody.velocity = new Vector2(runSpeed, rigidBody.velocity.y);
+            }
+
+            if (GetKeyDown(KeyCode.A)) {
+                rigidBody.velocity = new Vector2(-runSpeed, rigidBody.velocity.y);
+            }
+
             manaBar.transform.localScale = new Vector3(mana / maxMana, 1.0f, 1.0f);
         }
-        previousPosition = transform.position;
     }
 
     void Jump() {
-        rigidBody.velocity = new Vector2(rigidBody.velocity.x, 3);
+        if (grounded)
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpHeight);
     }
 
     void ToggleMenu() {
@@ -56,42 +87,77 @@ public class PlayerController : MonoBehaviour {
     }
 
     bool GetKeyPress(KeyCode key) {
-        if (disabledKeys.Find(x => x == key.ToString()) != null) {
-            return false;
+        if (disabledKeys.Find(x => x.Equals(key.ToString().ToLower())) != null) {
+            return DisabledKeyEffect(key);
         }
         return Input.GetKeyDown(key);
+    }
+
+    bool GetKeyDown(KeyCode key) {
+        if (disabledKeys.Find(x => x.Equals(key.ToString().ToLower())) != null) {
+            return DisabledKeyEffect(key);
+        }
+        return Input.GetKey(key);
+    }
+
+    public void SpendKey(string key) {
+        key = key.ToLower();
+        disabledKeys.Add(key);
+
+        switch(key) {
+            case "w":
+                AddMana(20);
+                break;
+            case "a":
+                AddMana(20);
+                break;
+            case "s":
+                AddMana(20);
+                break;
+            case "d":
+                AddMana(20);
+                break;
+            case "e":
+                AddMana(20);
+                break;
+            case "space":
+                AddMana(100);
+                break;
+        }
+    }
+
+    public void ReclaimKey(string key) {
+        key = key.ToLower();
+        disabledKeys.Remove(key);
+        UIButtons[key].GetComponent<DisableOnClick>().ReEnable();
     }
 
     void AddMana(float amount) {
         mana = Mathf.Clamp(mana + amount, 0.0f, maxMana);
     }
 
-    void GameOver() {
-        Debug.Log("YOU LOSE");
+    bool DisabledKeyEffect(KeyCode key) {
+        switch (key) {
+            case KeyCode.W:
+                return false;
+            case KeyCode.A:
+                return (int)Time.time % disabledKeys.Count == 0;
+            case KeyCode.S:
+                return false;
+            case KeyCode.D:
+                runSpeed = disabledKeys.Count;
+                return true;
+            case KeyCode.E:
+                return false;
+            case KeyCode.Space:
+                jumpHeight = disabledKeys.Count;
+                return true;
+            default:
+                return false;
+        }
     }
 
-    public void SpendKey(string key) {
-        disabledKeys.Add(key);
-
-        switch(key) {
-            case "W":
-                AddMana(20);
-                break;
-            case "A":
-                AddMana(20);
-                break;
-            case "S":
-                AddMana(20);
-                break;
-            case "D":
-                AddMana(20);
-                break;
-            case "E":
-                AddMana(20);
-                break;
-            case "Space":
-                AddMana(100);
-                break;
-        }
+    void GameOver() {
+        Debug.Log("YOU LOSE");
     }
 }
